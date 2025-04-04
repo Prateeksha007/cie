e6a8c97c28beea349aede1c1e32a6c3b893985ab
prateeksha_007 <royalriderr007@gmail.com>
2025-04-04 10:56:56 +0530

    save

---
+++ datatype.java
@@ -0,0 +1,20 @@
+public class datatype{
+    public static void main(String[]args){
+        int A = 10;
+        float B = 5.5;
+        double C = 2.5;
+        Char D = 'P';
+        String E = "Rider";
+        Byte F = 5;
+        Short G = 4;
+
+        System.out.println("int A value ="+A);
+        System.out.println("float B value ="+B);
+        System.out.println("double C value ="+C);
+        System.out.println("Char D value ="+D);
+        System.out.println("String E value ="+E);
+        System.out.println("Byte F value ="+F);
+        System.out.println("Short G value ="+G);
+
+    }
+}
